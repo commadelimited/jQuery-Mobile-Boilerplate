9ae1cf96bee6653c7e17685427573caf012136d9
Michael Martinez <machinedesigner@gmail.com>
2012-02-11 23:20:42 -0700

    Added some basic JQM overrides

--- js/mobileinit.js
+++ js/mobileinit.js
@@ -9,7 +9,26 @@
 */
 $(document).bind('mobileinit', function(event){
 	// apply overrides here
+    // Navigation
+    //$.mobile.page.prototype.options.backBtnText = "Go back";
+    //$.mobile.page.prototype.options.addBackBtn      = true;
+    //$.mobile.page.prototype.options.backBtnTheme    = "d";
 
+    // Page
+    //$.mobile.page.prototype.options.headerTheme = "a";  // Page header only
+    //$.mobile.page.prototype.options.contentTheme    = "c";
+    //$.mobile.page.prototype.options.footerTheme = "a";
 
-
+    // Listviews
+    //$.mobile.listview.prototype.options.headerTheme = "a";  // Header for nested lists
+    //$.mobile.listview.prototype.options.theme           = "c";  // List items / content
+    //$.mobile.listview.prototype.options.dividerTheme    = "d";  // List divider
+    //$.mobile.listview.prototype.options.splitTheme   = "c";
+    //$.mobile.listview.prototype.options.countTheme   = "c";
+    //$.mobile.listview.prototype.options.filterTheme = "c";
+    //$.mobile.listview.prototype.options.filterPlaceholder = "Filter data...";
+    
+    //$.mobile.dialog.prototype.options.theme
+    //$.mobile.selectmenu.prototype.options.menuPageTheme
+    //$.mobile.selectmenu.prototype.options.overlayTheme
 });
